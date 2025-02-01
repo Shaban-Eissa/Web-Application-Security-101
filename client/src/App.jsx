f55601eadf9037e90184cef0b7f5a89fe49b9c91
Shaban-Eissa <shabaneissa767@gmail.com>
2025-02-01 12:47:21 +0200

    style: add CSS styles for improved layout and design of the login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    const response = await axios.post('http://localhost:5000/login', { username, password });
+    const response = await axios.post("http://localhost:5000/login", {
+      username,
+      password,
+    });
     alert(response.data);
   };
   return (
-    <div>
+    <div className="container">
       <h1>Login</h1>
-      <input
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"
-      />
-      <br/> 
-      <input
-      type='password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <br/>
+      <div className="input-container">
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+      </div>
+      <div className="input-container">
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+      </div>
       <button onClick={handleLogin}>Login</button>
     </div>
   );
