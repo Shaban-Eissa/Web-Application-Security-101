36bce43764d6abc5498ab8e484cf53c8a745242e
Shaban-Eissa <shabaneissa767@gmail.com>
2025-02-02 11:16:19 +0200

    feat: enable cookie support for login requests by adding withCredentials option

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,10 +10,14 @@ function App() {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://localhost:5000/login", {
-        username,
-        password,
-      });
+      const response = await axios.post(
+        "http://localhost:5000/login",
+        {
+          username,
+          password,
+        },
+        { withCredentials: true }
+      );
 
       // Store the JWT token
       const token = response.data.token;
